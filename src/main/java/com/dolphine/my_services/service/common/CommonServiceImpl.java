f0a0159cac28f91f571b3e184282ec76862fd839
Binh Nguyen <ccloner02@gmail.com>
2017-05-05 00:05:21 +0700

    Release 2.5 fix 1.3

--- src/main/java/com/dolphine/my_services/service/common/CommonServiceImpl.java
+++ src/main/java/com/dolphine/my_services/service/common/CommonServiceImpl.java
@@ -24,11 +24,11 @@ public class CommonServiceImpl implements CommonService{
         try {
             byte[] bytes = file.getBytes();
             String path = context.getRealPath("/resources/images");
-            System.out.println(path);
+            System.out.println("Path:"+path);
             File dir =  new File(path);
             if(!dir.exists())
                 dir.mkdirs();
-            String sourceFile = path + "\\" + id +"n"+ file.getOriginalFilename();
+            String sourceFile = path + "/" + id +"n"+ file.getOriginalFilename();
             System.out.println(sourceFile);
             File serverFile  = new File(sourceFile);
             BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile,true));
