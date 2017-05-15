c501c2e7cd25919a968e614975f8046802b578aa
Binh Nguyen <ccloner02@gmail.com>
2017-05-15 16:06:15 +0700

    update dto

--- src/main/java/com/dolphine/my_services/dto/CustomerNoti.java
+++ src/main/java/com/dolphine/my_services/dto/CustomerNoti.java
@@ -1,7 +1,25 @@
 package com.dolphine.my_services.dto;
 
-/**
- * Created by PC on 5/15/2017.
- */
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@ToString
 public class CustomerNoti {
+    private int id;
+    private String content;
+    private Date sendDate;
+    private int customerId;
+
+    public CustomerNoti(int id, String content, Date sendDate, int customerId) {
+        this.id = id;
+        this.content = content;
+        this.sendDate = sendDate;
+        this.customerId = customerId;
+    }
+
+    public CustomerNoti() {
+    }
 }
